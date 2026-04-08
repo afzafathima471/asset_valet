e490c782940fcae07860206ba47e8b989e1ddc4e
afzafathima471 <afzaafzafathima@gmail.com>
2026-04-08 18:23:24 +0530

    Added psycopg2-binary

--- app/database.py
+++ app/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:shdje23$%^&df@db.dskuuydnlsfuzouppyom.supabase.co:5432/postgres"
+import os
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:shdje23$%^&df@db.dskuuydnlsfuzouppyom.supabase.co:5432/postgres")
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
